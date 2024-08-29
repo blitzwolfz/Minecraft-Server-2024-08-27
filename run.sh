b4f066c753c4c6c78e430fcde359a959c4d10e9f
Sam Q <saminm.qureshi@gmail.com>
2024-08-29 18:46:28 -0400

    Backup 2024-08-29 Part 2

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
 # screen -s java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.7/unix_args.txt "$@"
+echo "Script started at $(date)" >> /home/ubuntu/server/cron_debug.log
+whoami >> /home/ubuntu/server/cron_debug.log
+env >> /home/ubuntu/server/cron_debug.log
+
 screen -S Forge -d -m sh -c 'java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.7/unix_args.txt "$@"'
